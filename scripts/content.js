a52a678a94b5a25e198786aef6940b8b424a32f1
amax <alona.maksymova@atsr.ie>
2017-12-19 06:38:39 +0200

    added chart with day and night temperature

--- scripts/content.js
+++ scripts/content.js
@@ -1,19 +1,57 @@
 $(document).ready(function() {
-	// script = document.createElement('script');
-	// script.src = "https://apis.google.com/js/client.js?onload=init";
-	// (document.head||document.documentElement).appendChild(script);
-	// google.load("visualization", "1", {packages:["corechart"]});
-	 getWeather("Kherson");
+    // script.src = "https://apis.google.com/js/client.js?onload=init";
+    // (document.head||document.documentElement).appendChild(script);
+    // google.load("visualization", "1", {packages:["corechart"]});
 	var jsonOpenWeather = {};
 	var jsonDarkSky = {};
-	$("#show").on("click",function() {
-		var town = $(".town").val();
-		getWeather(town);
-	});
-});
+	getWeather("Kherson");
+    $("#show").on("click",function() {
+        var town = $(".town").val();
+        getWeather(town);
+    });
+//    google.setOnLoadCallback(drawChart);
+  });
+google.load("visualization", "1", {packages:["corechart"]});
+function drawChart() {
+	console.log(jsonDarkSky);
+    var data = new google.visualization.DataTable();
+    var dateArr = new Array();
+    var j=0;
+    data.addColumn('date', 'Date');
+    data.addColumn('number', 'Day');
+    data.addColumn('number', 'Night');
+    for (var i=1;i<6;i++){
+    	var mainDate = new Date();
+    	mainDate.setTime(jsonDarkSky.daily.data[i].temperatureHighTime*1000);
+    	dateArr.push([mainDate, fromFtoC(jsonDarkSky.daily.data[i].temperatureHigh), fromFtoC(jsonDarkSky.daily.data[i].temperatureLow)]);
+    }
+    data.addRows(dateArr);
+    var customFormatter = google.visualization.NumberFormat(
+
+    );
+    // Set chart options
+    var options = {
+        title: 'Next 5 day temperature',
+        width: "100%",
+        height: "100%",
+        hAxis: {
+            format: 'd/M'
+        },
+        chartArea:{
+            width: "85%"
+        },
+        legend: 'none',
+        colors: ['#ad4543','#3e43fe']
+    };
+    customFormatter.format(data, 2);
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafic'));
+    chart.draw(data, options);
+
+}
+
 function getWeather(city) {
 	getWeatherFromOpenWeather(city);
-
 }
 
 function getWeatherFromOpenWeather(city) {
@@ -67,6 +105,7 @@ function getWeatherFromDarkSky(lat,lon){
 		console.log(json.daily.icon);
 		$(".answer").append(" "+json.daily.icon);
 		showWeather();
+		drawChart();
 	}).fail(function(jqxhr, textStatus, error ) {
 		var err = textStatus + ", " + error;
 		console.log("Request Failed: " + err);
@@ -80,6 +119,7 @@ function showWeather() {
 	var statusDarkSky = jsonDarkSky.daily.icon
 	var temperatureOpenWeather = Math.round(jsonOpenWeather.list[0].main.temp);
 	var temperatureDarkSky = fromFtoC(jsonDarkSky.currently.temperature);
+	var averageTemperature = Math.round((temperatureOpenWeather + temperatureDarkSky) / 2);
 	if(statusOpenWeather == statusDarkSky){
 		$(".center_all").hide();
 		$(".center").show();
@@ -92,7 +132,7 @@ function showWeather() {
 	}
 	console.log(temperatureOpenWeather);
 	console.log(jsonDarkSky);
-	$(".temp0").find("span").text((temperatureOpenWeather>0 ? "+" : "" ) + temperatureOpenWeather);
+	$(".temp0").find("span").text((averageTemperature>0 ? "+" : "" ) + averageTemperature);
 	$(".temp1").find("span").text((temperatureOpenWeather>0 ? "+" : "" ) + temperatureOpenWeather);
 	$(".temp2").find("span").text((temperatureDarkSky>0 ? "+" : "") + temperatureDarkSky);
 	// setTemperature();
@@ -103,6 +143,7 @@ function setIconByStatus(status,img) {
 		$.each(data, function(key, val) {
 			if(key==status){
 				img.attr("src", "images/icons/"+val+".png")
+				img.attr("title", key)
 			}
 		});
 	});;
